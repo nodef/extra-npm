af7cd1858e5b611b717988c81881d78e8bc5ea60
Subhajit Sahu <wolfram77@gmail.com>
2018-07-22 08:07:10 +0530

    :snowman: linked push.sh <- index

--- index.sh
+++ index.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-npm "$@"
+# global variables
+cr="\033[0m"
+cm="\033[1;33m"
+
+psd() {
+  # present script directory
+  z="${BASH_SOURCE[0]}"
+  if [ -h "$z" ]; then z="$(readlink "$z")"; fi
+  cd "$(dirname "$0")" && cd "$(dirname "$z")" && pwd
+}
+
+# read arguments
+dp0="$(psd)/"
+if [[ "$1" == "push" ]]; then shift && source "${dp0}scripts/push.sh" "$@"
+else npm "$@"
+fi
