a9061fb0b246b29d9e743a7da08e719a65961393
Subhajit Sahu <wolfram77@gmail.com>
2018-07-28 14:00:33 +0530

    :snowman: let bundle have more comment beauty

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -6,14 +6,39 @@ const recast = require('recast');
 const fs = require('fs');
 
 
-function functionGetParams(ast, set) {
-  for(var p of ast.params) {
-    if(p.type==='Identifier') set.add(p.name);
-    else if(p.type==='RestElement') set.add(p.argument.name);
-    else if(p.type==='AssignmentPattern') set.add(p.left.name);
-  }
+
+// Get function parameter name.
+function paramName(ast) {
+  if(ast.type==='Identifier') return ast.name;
+  else if(ast.type==='RestElement') return ast.argument.name;
+  else if(ast.type==='AssignmentPattern') return ast.left.name;
+  return null;
+};
+
+// Check if statement is exports.
+function statementIsExports(ast) {
+  if(ast.type!=='ExpressionStatement') return false;
+  if(ast.expression.left.type!=='MemberExpression') return false;
+  return ast.expression.left.object.name==='exports';
+};
+
+function statementIsModuleExports(ast) {
+  if(ast.type!=='ExpressionStatement') return false;
+  if(ast.expression.left.type!=='MemberExpression') return false;
+  if(ast.expression.left.object.name!=='module') return false;
+  return ast.expression.left.property.name==='exports';
+};
+
+// Get function parameter names.
+// : helps exclude local identifiers.
+function functionParams(ast, set) {
+  for(var p of ast.params)
+    set.add(paramName(p));
   return set;
 };
+
+// Get empty map of window identifiers.
+// : first step of getting their locations.
 function bodyGetWindow(ast, map) {
   for(var s of ast) {
     if(s.type==='FunctionDeclaration') map.set(s.id.name, []);
@@ -24,10 +49,13 @@ function bodyGetWindow(ast, map) {
   }
   return map;
 };
+
+// Scan locations of window identifiers.
+// : helps rename when necessary.
 function bodyScanWindow(ast, map, exc) {
   if(ast==null || typeof ast!=='object') return map;
   if(/Function(Declaration|Expression)/.test(ast.type)) {
-    var excn = functionGetParams(ast, new Set(exc));
+    var excn = functionParams(ast, new Set(exc));
     return bodyScanWindow(ast.body, map, excn);
   }
   if(ast.type==='Identifier') {
@@ -37,37 +65,38 @@ function bodyScanWindow(ast, map, exc) {
   for(var k in ast) bodyScanWindow(ast[k], map, exc);
   return map;
 };
-function scriptScanWindow(ast) {
-  var body = ast.program.body;
-  var map = bodyGetWindow(body, new Map());
-  return bodyScanWindow(body, map, new Set());
-};
 
+// Rename a window identifier.
+// : help maintain unique globals.
 function windowRename(win, frm, to) {
   for(var ast of win.get(frm))
     ast.name = to;
   return win;
 };
-function globalsRename(glo, win, add) {
-  for(var [k, v] of win) {
-    if(!glo.has(k)) { glo.add(k); continue; }
-    else if(!glo.has(k+add)) {
-      windowRename(win, k, k+add);
-      glo.add(k+add); continue;
+
+// Add window identifiers to globals.
+// : they are renamed as necessary.
+function globalsAdd(glo, win, suf) {
+  for(var k of win.keys()) {
+    if(!glo.has(k)) glo.add(k);
+    else if(!glo.has(k+suf)) {
+      windowRename(win, k, k+suf);
+      glo.add(k+suf);
     }
-    for(var i=0; glo.has(k+add+i); i++) {}
-    windowRename(win, k, k+add,+i);
-    glo.add(k+add+i);
+    else {
+      for(var i=0; glo.has(k+suf+i); i++) {}
+      windowRename(win, k, k+suf+i);
+      glo.add(k+suf+i);
+    }
   }
   return glo;
 };
+
 function scriptUpdateExports(ast, exp) {
   var body = ast.program.body, idx = -1;
   for(var i=0, I=body.length; i<I; i++) {
     var s = body[i];
-    if(s.type!=='ExpressionStatement') continue;
-    if(s.expression.left.type!=='MemberExpression') continue;
-    if(s.expression.left.object.name!=='exports') continue;
+    if(!statementIsExports(s)) continue;
     s.expression.left.object.name = exp;
     if(idx<0) idx = i;
   }
@@ -80,10 +109,7 @@ function scriptUpdateModuleExports(ast, exp) {
   var body = ast.program.body, idx = -1, right = null;
   for(var i=0, I=body.length; i<I; i++) {
     var s = body[i];
-    if(s.type!=='ExpressionStatement') continue;
-    if(s.expression.left.type!=='MemberExpression') continue;
-    if(s.expression.left.object.name!=='module') continue;
-    if(s.expression.left.property.name!=='exports') continue;
+    if(!statementIsModuleExports(s)) continue;
     right = s.expression.right; I--;
     body.splice(idx=i--, 1);
   }
@@ -95,13 +121,38 @@ function scriptUpdateModuleExports(ast, exp) {
   return exp;
 };
 
+function bodyUpdateRequire(exp, astp, astc, paths) {
+  if(astc==null || typeof astc!=='object') return exp;
+  if(astc.type==='CallExpression' && astc.callee.name==='require') {
+    var id = require.resolve(astc.arguments[0].value, {paths}), left = null;
+    if(astp.type==='VariableDeclarator') left = astp.id.name;
+    else if(astp.type==='AssignmentExpression') left = astp.left.name;
+    if(!exp.has(id)) {
+      var txt = fs.readFileSync(id, 'utf8');
+      var astr = recast.parse(txt);
+      
+      // load require here
+    }
+    if(astp.type==='VariableDeclarator') astp.init = exp.get(id);
+    else if(astp.type==='AssignmentExpression') astp.right = exp.get(id);
+  }
+  for(var k in astc) bodyUpdateRequire(exp, astc, astc[k], paths);
+  return exp;
+};
+
+function scriptScanWindow(ast) {
+  var body = ast.program.body;
+  var map = bodyGetWindow(body, new Map());
+  return bodyScanWindow(body, map, new Set());
+};
+
 function scriptProcess(sym, ast, add, del=false) {
   var win = scriptScanWindow(ast);
   console.log('win', win);
   var exp = scriptUpdateExports(ast, add);
   exp = exp||scriptUpdateModuleExports(ast, add);
   console.log('exp', exp);
-  globalsRename(sym.globals, win, add);
+  globalsAdd(sym.globals, win, add);
   sym.exports.set(add, exp);
   return sym;
 };
