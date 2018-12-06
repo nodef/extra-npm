7503d92d580518ee7d8dd6e3f1f1191d099050b1
Subhajit Sahu <wolfram77@gmail.com>
2018-12-06 21:38:49 +0530

    show: enable cli versions, contents, dependents

--- show.js
+++ show.js
@@ -1,31 +1,74 @@
+const npmPackageVersions = require('npm-package-versions');
 const listNpmContents = require('list-npm-contents');
 const moduleDependents = require('module-dependents');
+const boolean = require('boolean');
 
+
+// Global variables.
+const E = process.env;
+const FUNCTION = new Map([
+  ['versions', versions],
+  ['contents', contents],
+  ['dependents', dependents]
+]);
+const OPTIONS = {
+  log: boolean(E['ENPM_LOG']||'0'),
+  count: boolean(E['ENPM_COUNT']||'0')
+};
+
+
+// Get versions of package.
+function versions(pkg, fn) {
+  if(pkg.includes('@')) fn(null, [pkg.replace(/.*?@/, '')]);
+  npmPackageVersions(pkg, fn);
+};
 
 // Get contents of package.
-async function contents(pkg, o={}) {
+function contents(pkg, fn) {
   var nam = pkg.replace(/@.*/, '');
   var ver = pkg.includes('@')? pkg.replace(/.*?@/, ''):null;
-  try {
-    var a = await listNpmContents(nam, ver);
-    if(!o.raw) return console.log(a);
-    for(var f of a)
-      console.log(f);
-  }
-  catch(e) { console.error(`error: package "${pkg}" not found`); }
+  listNpmContents(nam, ver).then((fils) => fn(null, fils), fn);
 };
 
 // Get dependents of package.
-function dependents(pkg, o={}) {
+function dependents(pkg, fn) {
   var a = [];
-  moduleDependents(pkg).on('error', err => {
-    console.error('error:', err);
-  }).on('data', p => a.push(p.name)).on('end', () => {
-    if(!o.raw) return console.log(a);
-    for(var p of a)
-      console.log(p);
+  var r = moduleDependents(pkg)
+  r.on('error', fn)
+  r.on('data', p => a.push(p.name));
+  r.on('end', () => fn(null, a));
+};
+
+// Show details of package.
+function show(typ, pkg, o) {
+  var fn = FUNCTION.get(typ);
+  if(fn==null) return console.error(`error: unknown field "${typ}"`);
+  fn(pkg, (err, ans) => {
+    if(err) return console.error('error:', err.message||`package "${pkg}" not found`);
+    if(o.log) console.log(`${pkg} has ${ans.length} ${typ}`);
+    if(o.count) { if(!o.log) console.log(ans.length); return; }
+    if(o.log) return console.log(ans);
+    for(var v of ans)
+      console.log(v);
   });
 };
 
-exports.contents = contents;
-exports.dependents = dependents;
+show.versions = versions;
+show.contents = contents;
+show.dependents = dependents;
+module.exports = show;
+
+// Command line.
+function shell(a) {
+  var typs = [], pkg = null, o = OPTIONS;
+  for(var i=2, I=a.length; i<I; i++) {
+    if(a[i]==='-l' || a[i]==='--log') o.log = true;
+    else if(a[i]==='-c' || a[i]==='--count') o.count = true;
+    else if(pkg==null) pkg = a[i];
+    else if(a[i].startsWith(':')) typs.push(a[i].substring(1));
+  }
+  if(!pkg) return;
+  for(var t of typs)
+    show(t, pkg, o);
+};
+if(require.main===module) shell(process.argv);
