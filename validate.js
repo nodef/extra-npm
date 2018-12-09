034bc300bb570cd14eae69d4014110df2e8eaa02
Subhajit Sahu <wolfram77@gmail.com>
2018-12-09 22:25:56 +0530

    colorify validate

--- validate.js
+++ validate.js
@@ -2,15 +2,18 @@
 const validateNpmPackageName = require('validate-npm-package-name');
 const validateNpmPackageLicense = require('validate-npm-package-license');
 const npmUserValidate = require('npm-user-validate');
-const semver = require('semver');
 const boolean = require('boolean');
+const semver = require('semver');
+const kleur = require('kleur');
+const cp = require('child_process');
 
 
 // Global variables.
+const E = process.env;
 const OPTIONS = {
   type: null,
   value: null,
-  log: boolean(E['ENPM_VALIDATE_LOG']||E['ENPM_LOG']||'1')
+  silent: boolean(E['ENPM_VALIDATE_SILENT']||E['ENPM_SILENT']||'0')
 };
 const FUNCTION = new Map([
   ['name', name],
@@ -19,6 +22,7 @@ const FUNCTION = new Map([
   ['email', email],
   ['username', username],
 ]);
+const STDIO = [0, 1, 2];
 
 
 // Validate package name.
@@ -26,19 +30,19 @@ function name(txt, o) {
   var a = validateNpmPackageName(txt);
   if(a.validForNewPackages) return console.log(1);
   if(a.validForOldPackages) return console.log(0);
-  if(!o.log) return console.log(-1);
+  if(o.silent) return console.log(-1);
   for(var m of a.errors||[])
-    console.error('error:', m);
+    console.error(kleur.red('error:'), m);
   for(var m of a.warnings||[])
-    console.warn('warning:', m);
+    console.warn(kleur.yellow('warning:'), m);
 };
 
 // Validate package version.
 function version(txt, o) {
   var a = semver.valid(txt);
   if(a) return console.log(1);
-  if(!o.log) return console.log(-1);
-  console.error('error: invalid semver format');
+  if(o.silent) return console.log(-1);
+  console.error(kleur.red('error:'), 'invalid semver format');
 };
 
 // Validate package license.
@@ -46,38 +50,40 @@ function license(txt, o) {
   var a = validateNpmPackageLicense(txt);
   if(a.validForNewPackages) return console.log(1);
   if(a.validForOldPackages) return console.log(0);
-  if(!o.log) return console.log(-1);
+  if(o.silent) return console.log(-1);
   for(var m of a.warnings||[])
-    console.warn('warning:', m);
+    console.warn(kleur.yellow('warning:'), m);
 };
 
 // Validate email.
 function email(txt, o) {
   var e = npmUserValidate.email(txt);
   if(e==null) return console.log(1);
-  if(!o.log) return console.log(-1);
-  console.error('error:', e.message);
+  if(o.silent) return console.log(-1);
+  console.error(kleur.red('error:'), e.message);
 };
 
 // Validate username.
 function username(txt, o) {
   var e = npmUserValidate.username(txt);
   if(e==null) return console.log(1);
-  if(!o.log) return console.log(-1);
-  console.error('error:', e.message);
+  if(o.silent) return console.log(-1);
+  console.error(kleur.red('error:'), e.message);
 };
 
 // Validate something, dont leave to chance!
 function validate(typ, txt, o) {
   var o = Object.assign({}, OPTIONS, o);
   var fn = FUNCTION.get(typ);
-  return fn? fn(txt, o):console.log('error: cannot validate '+typ)
+  if(fn!=null) return fn(txt, o);
+  if(o.silent) return console.log(-1);
+  console.error(kleur.red('error:'), 'cannot validate '+typ);
 };
 
 // Get options from arguments.
 function options(o, k, a, i) {
   if(k==='-help') o.help = true;
-  else if(k==='-l' || k==='--log') o.log = true;
+  else if(k==='--silent') o.silent = true;
   else if(!o.type) o.type = a[i];
   else o.value = a[i];
   return i+1;
@@ -91,6 +97,7 @@ function shell(a) {
   var o = {};
   for(var i=2, I=a.length; i<I;)
     i = options(o, a[i], a, i);
+  if(o.help) return cp.execSync('less README.md', {cwd: __dirname, stdio: STDIO});
   validate(o.type, o.value, o);
 };
 if(require.main===module) shell(process.argv);
