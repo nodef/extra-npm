f2a19e313ae1e9ed0c328bf4ff3b37c1aeef478a
Subhajit Sahu <wolfram77@gmail.com>
2020-01-04 22:26:25 +0530

    save

--- scripts/which.js
+++ scripts/which.js
@@ -3,10 +3,16 @@ const npmWhich = require('npm-which');
 const boolean = require('boolean');
 const kleur = require('kleur');
 const cp = require('child_process');
+const path = require('path');
+const os = require('os');
 
 
 // Global variables.
 const E = process.env;
+const PLATFORM = os.platform();
+const PATH = E['PATH']||'';
+const PATHEXT = E['PATHEXT']||'';
+const PATHSEP = PLATFORM==='win32'? ';':':';
 const OPTIONS = {
   help: false,
   cwd: null,
@@ -31,6 +37,32 @@ function which(cmd, o) {
     console.log(pth);
   });
 };
+
+function _package(dir, fn) {
+  dir = dir.startsWith('/')? dir:path.resolve(dir);
+  var pkg = path.join(dir, 'package.json');
+  fs.access(pkg, err => {
+    if(!err) return fn(dir);
+    if(path.isAbsolute(dir)) return fn(null);
+    _package(path.dirname(dir), fn);
+  });
+}
+function which(cmd) {
+  for(var p of PATH.split(PATHSEP)) {
+    
+  }
+}
+
+function whichWin32(cmd) {
+  
+}
+function isExeWin(pth) {
+  var ext = path.extname(pth);
+  if(PATHEXT.includes(ext)) return true;
+}
+
+
+
 
 // Get options from arguments.
 function options(o, k, a, i) {
