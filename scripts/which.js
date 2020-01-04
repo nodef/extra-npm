4687060f944bbd3bc35fe4e301e1b79d39e132fb
Subhajit Sahu <wolfram77@gmail.com>
2020-01-05 01:17:17 +0530

    :mushroom: whichDir now works

--- scripts/which.js
+++ scripts/which.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
 const npmWhich = require('npm-which');
-const boolean = require('boolean');
 const kleur = require('kleur');
 const cp = require('child_process');
 const path = require('path');
 const os = require('os');
+const fs = require('fs');
+require('extra-boolean');
 
 
 // Global variables.
 const E = process.env;
-const PLATFORM = os.platform();
 const PATH = E['PATH']||'';
-const PATHEXT = E['PATHEXT']||'';
-const PATHSEP = PLATFORM==='win32'? ';':':';
+const PATHEXT = (E['PATHEXT']||'').toLowerCase();
+const WIN32 = os.platform()==='win32';
+const PATHSEP = WIN32? ';':':';
 const OPTIONS = {
   help: false,
   cwd: null,
   command: null,
-  silent: boolean(E['ENPM_SILENT']||'0')
+  silent: Boolean.parse(E['ENPM_SILENT']||'0')
 };
 const STDIO = [0, 1, 2];
 
@@ -29,14 +30,14 @@ function error(err, o) {
 };
 
 // Get path of local bin.
-function which(cmd, o) {
-  var o = Object.assign({}, OPTIONS, o);
-  var cwd = o.cwd||process.cwd();
-  npmWhich(cwd)(cmd, (err, pth) => {
-    if(err) return error(err, o);
-    console.log(pth);
-  });
-};
+// function which(cmd, o) {
+//   var o = Object.assign({}, OPTIONS, o);
+//   var cwd = o.cwd||process.cwd();
+//   npmWhich(cwd)(cmd, (err, pth) => {
+//     if(err) return error(err, o);
+//     console.log(pth);
+//   });
+// };
 
 function _package(dir, fn) {
   dir = dir.startsWith('/')? dir:path.resolve(dir);
@@ -47,22 +48,51 @@ function _package(dir, fn) {
     _package(path.dirname(dir), fn);
   });
 }
-function which(cmd) {
-  for(var p of PATH.split(PATHSEP)) {
-    
+
+
+function which(prog) {
+  return new Promise((fres, frej) => whichOs(prog, fres));
+}
+
+function whichNode() {
+
+}
+
+function whichOs(prog, fn) {
+  var map = new Map();
+  var ps = PATH.split(PATHSEP);
+  for(var i=0, j=0, I=ps.length; i<I; i++) {
+    whichDir(prog, ps[i], map, err => {
+      if(++j>=I) fn(map.values());
+    });
   }
 }
 
-function whichWin32(cmd) {
-  
+
+
+function whichDir(prog, dir, map, fn) {
+  var isExe = WIN32? isExeWin32:isExeNix;
+  fs.readdir(dir, {withFileTypes: true}, (err, files) => {
+    if(err) return fn(err);
+    for(var f of files) {
+      if(!f.isFile()) continue;
+      var base = path.basename(f.name);
+      var p = path.join(dir, f.name);
+      var p0 = map.get(base)||null;
+      if(!prog.test(base)) continue;
+      if(isExe(p, p0)) map.set(base, p);
+    }
+    fn(null);
+  });
 }
-function isExeWin(pth) {
-  var ext = path.extname(pth);
-  if(PATHEXT.includes(ext)) return true;
+function isExeWin32(p1, p0) {
+  var i0 = PATHEXT.indexOf(path.extname(p0||'f.zzz').toLowerCase());
+  var i1 = PATHEXT.indexOf(path.extname(p1).toLowerCase());
+  return i1>=0 && (i0<0 || i1<i0);
 }
-
-
-
+function isExeNix(p1) {
+  return path.extname(p1)==='';
+}
 
 // Get options from arguments.
 function options(o, k, a, i) {
