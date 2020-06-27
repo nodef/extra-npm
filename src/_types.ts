1ad2049f3bfb60d084db071b4fc0758291967028
Subhajit Sahu <wolfram77@gmail.com>
2020-06-27 20:48:47 +0530

    :coconut: chutney ;configPath

--- src/_types.ts
+++ src/_types.ts
@@ -1,4 +1,6 @@
 export interface IConfig {
+  /** Selected npmrc type (project/user/global/builtin) [user]. */
+  type?: string,
   /** Path to project npmrc. */
   projectconfig?: string,
   /** Path to user npmrc. */
--- src/configPath.ts
+++ src/configPath.ts
@@ -16,15 +16,16 @@ function configPathFind(): void {
 
 /**
  * Gets path to npmrc file.
- * @param typ path type (project/user/global/builtin) [user]
  * @param opt options
  */
-function configPath(typ: string='user', opt: IConfig=null, ref: boolean=false): string {
-  var o = opt || {};
+function configPath(opt: IConfig=null, ref: boolean=false): string {
   if(ref || !PBUILTIN) configPathFind();
-  if(typ==='project') return o.projectconfig || null;
-  if(typ==='global') return o.globalconfig || PGLOBAL;
-  if(typ==='builtin') return PBUILTIN;
-  return o.userconfig || PUSER;
+  var o = opt||{};
+  switch(o.type) {
+    case 'project': return o.projectconfig || null;
+    case 'global':  return o.globalconfig || PGLOBAL;
+    case 'builtin': return PBUILTIN;
+    default: return o.userconfig || PUSER;
+  }
 }
 export default configPath;
